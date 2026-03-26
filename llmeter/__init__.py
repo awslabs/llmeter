672f3484607ab61e02df539e9b5523e4ac373942
Alex Thewsey <thewsey@amazon.com>
2026-03-26 12:10:24 +0800

    refactor: Fetch version from generic __name__
    Use the __name__ variable to retrieve LLMeter's version from
    importlib, rather than hard-coding the module's name.

--- llmeter/__init__.py
+++ llmeter/__init__.py
@@ -1,6 +1,6 @@
 from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = version("llmeter")
+    __version__ = version(__name__)
 except PackageNotFoundError:
     __version__ = "0.0.0"
